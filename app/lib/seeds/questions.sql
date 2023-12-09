c4c2c1f4952f42c0d8d4096091666a0224a80f81
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-09 09:59:40 +0100

    Questions file complete.

---
+++ app/lib/seeds/questions.sql
@@ -0,0 +1,433 @@
+-- SEEDS
+-- Questions
+
+INSERT INTO Questions ( -- First name / native
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    'e118c30f-3a4a-4cb3-bf9a-67d04dc177a4',
+    'First name',
+    'LIVE',
+    'NATIVE',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Work number / native
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '7de346e6-dc73-4d68-b6a3-abb5d09654cc',
+    'Work number',
+    'LIVE',
+    'NATIVE',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Email address / native
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    'b80f6893-f013-4964-b770-6935ef8fc4a4',
+    'Email address',
+    'LIVE',
+    'NATIVE',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Other email address / native
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    'ba3a314a-98a4-419d-a0c7-6d9eab5ac2cf',
+    'Other email address',
+    'LIVE',
+    'NATIVE',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Last name / native / irl
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '2a389094-1259-4a3d-8126-8635aeab39fc',
+    'Last name',
+    'LIVE',
+    'NATIVEIRL',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Phone number / native / irl
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '97280672-cde7-47d0-9795-da926ecb9a79',
+    'Phone number',
+    'LIVE',
+    'NATIVEIRL',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Address / native / irl
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '6e451910-5f31-4649-9451-ad135663e71d',
+    'Address',
+    'LIVE',
+    'NATIVEIRL',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Other address / native / irl
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '05e01dae-1884-478c-aa19-2231355948ee',
+    'Other address',
+    'LIVE',
+    'NATIVEIRL',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Birthday / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '34ce07f9-03c3-41b9-b36d-26bbebcf4998',
+    'Birthday',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Birthdate / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '4b7bcb0d-2eb1-4783-a762-7fbfb7106f6d',
+    'Birthdate',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Father's birthday / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '1cc96549-4eab-4c97-89b4-3945866d4f39',
+    'Father''s birthday',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Mother's birthday / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '1eb27595-094d-421e-a508-14b53cf99c42',
+    'Mother''s birthday',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Girlfriend's birthday / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    'ec7370c8-18ad-4df3-85da-96d141c1a67e',
+    'Girlfriend''s birthday',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Crush's birthday / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '40b2ecf3-397c-4be0-81f4-99363a4d1a53',
+    'Crush''s birthday',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Father's birthdate / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '815a9b18-0894-42e4-8fa2-68389329ac8d',
+    'Father''s birthdate',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Mother's birthdate / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '4caf41b5-61af-4f69-be69-f6e74c899ed9',
+    'Mother''s birthdate',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Girlfriend's birthdate / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '73fe5a9d-3bc2-477d-abe4-bda3efaf7573',
+    'Girlfriend''s birthdate',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Crush's birthdate / pseudo
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '7052b6ca-28c7-4a8f-859d-c93dc6db36cc',
+    'Crush''s birthdate',
+    'LIVE',
+    'PSEUDO',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite actor / custom
+    question_id,
+    user_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    'ffb298c5-e038-44f9-8704-21182691b34e',
+    'e17bc7f7-b93f-4915-9f72-83d055c66e77',
+    'Favorite actor',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite actress / custom
+-- !! Former 'Favorite series' !! --
+    question_id,
+    user_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '1dbf7b3b-2a0a-4a9b-bba8-40aa74fcf874',
+    '2640aaf6-20b5-497c-b980-fbee374830c2',
+    'Favorite actress',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite anime character / custom
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '7e1cdcaa-8a34-4e37-8648-ecbc0480c60c',
+    'Favorite anime character',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite anime waifu / custom
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '48b6148e-4480-4d7e-b538-2b7ef5643342',
+    'Favorite anime waifu',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite anime series / custom
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '98f11a85-08c7-43b9-8c58-b62725da6294',
+    'Favorite anime series',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite anime franchise / custom
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    'a674ba4b-b26b-4b81-990d-891cbc337503',
+    'Favorite anime franchise',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
+INSERT INTO Questions ( -- Favorite anime studio / custom
+    question_id,
+    question_name,
+    question_state,
+    question_kind,
+    question_created_at,
+    question_updated_at
+)
+VALUES (
+    '6edc911c-848b-4f69-b45e-2308d80b1c25',
+    'Favorite anime studio',
+    'LIVE',
+    'CUSTOM',
+    now(),
+    now()
+);
+
