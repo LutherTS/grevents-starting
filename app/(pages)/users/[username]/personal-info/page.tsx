9438f771bce5d87b15e10fa484711a49858a5378
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-10 07:37:20 +0100

    ...I need a userquestion_pinned_at for sorting.

--- app/(pages)/users/[username]/personal-info/page.tsx
+++ app/(pages)/users/[username]/personal-info/page.tsx
@@ -17,6 +17,36 @@ async function fetchUserByUsername(username: string) {
     throw new Error("Failed to fetch user data.");
   }
 }
+
+async function fetchUserPinnedAnswers(user_id: string) {
+  console.log(user_id);
+  try {
+    const data = await sql`
+      SELECT Questions.question_name, Answers.answer_value, Answers.answer_id FROM Answers 
+      JOIN UserQuestions ON Answers.userquestion_id = UserQuestions.userquestion_id
+      JOIN Questions ON UserQuestions.question_id = Questions.question_id
+      JOIN Users ON Answers.user_id = Users.user_id
+
+      WHERE UserQuestions.user_id = '${user_id}'
+      AND Answers.user_id = '${user_id}'
+      AND UserQuestions.userquestion_is_pinned = TRUE
+
+      AND Answers.answer_state = 'LIVE'
+      AND UserQuestions.userquestion_state = 'LIVE'
+      AND Questions.question_state = 'LIVE'
+      AND Users.user_state = 'LIVE'
+
+      ORDER BY Answers.answer_updated_at
+      LIMIT 1
+      ;
+    `;
+    console.log(data);
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch user data.");
+  }
+}
 */
 
 export default async function PersonalInfo({
