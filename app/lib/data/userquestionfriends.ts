b264cae3f40cdd3323a1e8d8fb6a77f966e8e11b
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-10 16:03:38 +0100

    Onto that pull request.

--- app/lib/data/userquestionfriends.ts
+++ app/lib/data/userquestionfriends.ts
@@ -1,6 +1,6 @@
 import { sql } from "@vercel/postgres";
-import { unstable_noStore as noStore } from "next/cache";
 import { Answer } from "../definitions/answers";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function countUserQuestionFriends(answer: Answer) {
   noStore();
