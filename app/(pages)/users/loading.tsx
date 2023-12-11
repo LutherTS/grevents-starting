a2a9fcb1658fcbcca38eff488f94d71ec4789850
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-11 14:06:17 +0100

    Unplugged and added loading.

---
+++ app/(pages)/users/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="min-h-screen px-8 py-32 w-full flex justify-center items-center">
+        <h1>Loading...</h1>
+      </div>
+    </>
+  );
+}
