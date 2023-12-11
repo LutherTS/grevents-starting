312a29ab306d23a0717060b0dd5347594f49e86a
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-11 19:27:04 +0100

    Everything works, noStore restored for production.

--- app/(pages)/users/[username]/personal-info/standardized/page.tsx
+++ app/(pages)/users/[username]/personal-info/standardized/page.tsx
@@ -1,9 +1,9 @@
-// import { fetchUserByUsername } from "@/app/lib/data/users";
-// import {
-//   ManyUserNativeNotIrlCriteria,
-//   ManyUserNativeIrlCriteria,
-// } from "@/app/components/server/answers";
-// import { notFound } from "next/navigation";
+import { fetchUserByUsername } from "@/app/lib/data/users";
+import {
+  ManyUserNativeNotIrlCriteria,
+  ManyUserNativeIrlCriteria,
+} from "@/app/components/server/answers";
+import { notFound } from "next/navigation";
 import { PageLink } from "@/app/components/agnostic/links";
 
 export default async function StardardizedPage({
@@ -14,21 +14,21 @@ export default async function StardardizedPage({
   };
 }) {
   const username = params.username;
-  // const user = await fetchUserByUsername(username);
+  const user = await fetchUserByUsername(username);
 
-  // if (!user) {
-  //   notFound();
-  // }
+  if (!user) {
+    notFound();
+  }
 
   return (
     <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
       <div className="max-w-prose text-center">
-        <h1>Welcome to {username}&apos;s Standardized Info.</h1>
-        {/* <h1 className="font-semibold">
+        {/* <h1>Welcome to {username}&apos;s Standardized Info.</h1> */}
+        <h1 className="font-semibold">
           Welcome to {user.user_app_wide_name}&apos;s Standardized Info.
         </h1>
         <ManyUserNativeNotIrlCriteria user={user} />
-        <ManyUserNativeIrlCriteria user={user} /> */}
+        <ManyUserNativeIrlCriteria user={user} />
         <PageLink
           href={`/users/${username}/personal-info/customized`}
           name={"To Customized criteria"}
