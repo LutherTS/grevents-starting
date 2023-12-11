aedb48770d9144d9906a84022a1ec87096af21fd
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-11 08:51:02 +0100

    Let's retry without async on Criteria.

--- app/ui/components/Criteria.tsx
+++ app/ui/components/Criteria.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CriteriaAnswer, CriteriaQuestion } from "./answers";
 
-export async function Criteria({ answer }: { answer: Answer }) {
+export function Criteria({ answer }: { answer: Answer }) {
   const pathname = usePathname();
   const answerCustomizedPath = `/users/${answer.user_username}/personal-info/customized`;
 
