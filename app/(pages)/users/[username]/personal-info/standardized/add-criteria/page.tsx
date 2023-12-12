cb37637f8817021ab8277147e5c6a25d7557b91d
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-12 07:55:57 +0100

    Add and modify pages, with Questions requests done.

---
+++ app/(pages)/users/[username]/personal-info/standardized/add-criteria/page.tsx
@@ -0,0 +1,74 @@
+import { fetchUserByUsername } from "@/app/lib/data/users";
+import {
+  fetchAllNativeNotIrlQuestions,
+  fetchAllNativeIrlQuestions,
+} from "@/app/lib/data/questions";
+import { notFound } from "next/navigation";
+import { PageLink } from "@/app/components/agnostic/links";
+
+export default async function AddCriteriaStandardizedPage({
+  params,
+}: {
+  params: {
+    username: string;
+  };
+}) {
+  const username = params.username;
+  const user = await fetchUserByUsername(username);
+  const allNativeNotIrlQuestions = await fetchAllNativeNotIrlQuestions();
+  const allNativeIrlQuestions = await fetchAllNativeIrlQuestions();
+
+  if (!user) {
+    notFound();
+  }
+
+  return (
+    <>
+      <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
+        <div className="max-w-prose text-center">
+          {/* <h1>Welcome to {username}&apos;s Add Criteria Standardized.</h1> */}
+          <h1 className="font-semibold">
+            Welcome to {user.user_app_wide_name}&apos;s Add Criteria
+            Standardized.
+          </h1>
+          {allNativeNotIrlQuestions.length > 0 && (
+            <>
+              <p className="pt-2">Select a native question below</p>
+              <ol>
+                {allNativeNotIrlQuestions.map((nativeNotIrlQuestion) => {
+                  return (
+                    <li key={nativeNotIrlQuestion.question_id}>
+                      <p className="pt-2">
+                        {nativeNotIrlQuestion.question_name} / native
+                      </p>
+                    </li>
+                  );
+                })}
+              </ol>
+            </>
+          )}
+          {allNativeIrlQuestions.length > 0 && (
+            <>
+              <p className="pt-2">Select a native irl question below</p>
+              <ol>
+                {allNativeIrlQuestions.map((nativeIrlQuestion) => {
+                  return (
+                    <li key={nativeIrlQuestion.question_id}>
+                      <p className="pt-2">
+                        {nativeIrlQuestion.question_name} / native / irl
+                      </p>
+                    </li>
+                  );
+                })}
+              </ol>
+            </>
+          )}
+          <PageLink
+            href={`/users/${username}/personal-info/standardized`}
+            name={"Cancel"}
+          />
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ app/lib/data/questions.ts
@@ -0,0 +1,54 @@
+import { sql } from "@vercel/postgres";
+import {
+  NativeNotIrlQuestion,
+  NativeIrlQuestion,
+} from "../definitions/questions";
+// import { unstable_noStore as noStore } from "next/cache";
+
+export async function fetchAllNativeNotIrlQuestions() {
+  // noStore();
+  try {
+    const data = await sql<NativeNotIrlQuestion>`
+      SELECT 
+        question_name,
+        question_kind,
+        question_id
+      FROM Questions
+
+      WHERE question_kind = 'NATIVE'
+      
+      AND question_state = 'LIVE'
+
+      LIMIT 10;
+    `;
+    // console.log(data);
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch native not irl questions.");
+  }
+}
+
+export async function fetchAllNativeIrlQuestions() {
+  // noStore();
+  try {
+    const data = await sql<NativeIrlQuestion>`
+      SELECT 
+        question_name,
+        question_kind,
+        question_id
+      FROM Questions
+
+      WHERE question_kind = 'NATIVEIRL'
+      
+      AND question_state = 'LIVE'
+
+      LIMIT 10;
+    `;
+    // console.log(data);
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch native irl questions.");
+  }
+}
