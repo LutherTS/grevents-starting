899b7b1801df8a007e27caf25d17bd3e67d7a19e
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-12 13:18:00 +0100

    Preview pages made and linked to /previews.

---
+++ app/(pages)/users/[username]/previews/friend/page.tsx
@@ -0,0 +1,30 @@
+// import { fetchUserByUsername } from "@/app/lib/data/users";
+// import { notFound } from "next/navigation";
+import { PageLink } from "@/app/components/agnostic/links";
+
+export default async function FriendPreviewPage({
+  params,
+}: {
+  params: {
+    username: string;
+  };
+}) {
+  const username = params.username;
+  // const user = await fetchUserByUsername(username);
+
+  // if (!user) {
+  //   notFound();
+  // }
+
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
+      <div className="max-w-prose text-center">
+        <h1>Welcome to {username}&apos;s Friend Preview.</h1>
+        {/* <h1 className="font-semibold">
+          Welcome to {user.user_app_wide_name}&apos;s Friend Preview.
+        </h1> */}
+        <PageLink href={`/users/${username}/previews`} name={"To Previews"} />
+      </div>
+    </main>
+  );
+}
