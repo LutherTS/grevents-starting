fb0a4dc06c430cda34bc1ddf8623b5287b84f28d
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-12 11:00:34 +0100

    First Suspense implemented.

--- app/(pages)/users/[username]/personal-info/customized/modify-criteria/page.tsx
+++ app/(pages)/users/[username]/personal-info/customized/modify-criteria/page.tsx
@@ -1,9 +1,9 @@
-// import { fetchUserByUsername } from "@/app/lib/data/users";
-// import {
-//   ManyUserPseudonativeNotIrlCriteria,
-//   ManyUserPseudonativeIrlCriteria,
-// } from "@/app/components/server/answers";
-// import { notFound } from "next/navigation";
+import { fetchUserByUsername } from "@/app/lib/data/users";
+import {
+  ManyUserPseudonativeNotIrlCriteria,
+  ManyUserPseudonativeIrlCriteria,
+} from "@/app/components/server/answers";
+import { notFound } from "next/navigation";
 import { PageLink } from "@/app/components/agnostic/links";
 
 export default async function ModifyCriteriaCustomizedPage({
@@ -14,17 +14,17 @@ export default async function ModifyCriteriaCustomizedPage({
   };
 }) {
   const username = params.username;
-  // const user = await fetchUserByUsername(username);
+  const user = await fetchUserByUsername(username);
 
-  // if (!user) {
-  //   notFound();
-  // }
+  if (!user) {
+    notFound();
+  }
 
   return (
     <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
       <div className="max-w-prose text-center">
-        <h1>Welcome to {username}&apos;s Modify Criteria Customized.</h1>
-        {/* <h1 className="font-semibold">
+        {/* <h1>Welcome to {username}&apos;s Modify Criteria Customized.</h1> */}
+        <h1 className="font-semibold">
           Welcome to {user.user_app_wide_name}&apos;s Modify Criteria
           Customized.
         </h1>
@@ -33,7 +33,7 @@ export default async function ModifyCriteriaCustomizedPage({
         <p className="pt-2">
           (Custom criteria have their own dynamic modify page directly available
           from the parent Customized criteria page.)
-        </p> */}
+        </p>
         <PageLink
           href={`/users/${username}/personal-info/customized`}
           name={"Cancel"}
