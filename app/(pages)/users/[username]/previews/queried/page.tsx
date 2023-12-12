60fbe5ebf4657ff730fbb6b31ade593c2456f99c
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-12 22:55:27 +0100

    Jesus Christ it's working. Goodnight.

--- app/(pages)/users/[username]/previews/queried/page.tsx
+++ app/(pages)/users/[username]/previews/queried/page.tsx
@@ -1,12 +1,13 @@
-// import { fetchUserByUsername } from "@/app/lib/data/users";
-// import { gatherContactByUserAndUsername } from "@/app/lib/data/contacts";
-// import { notFound } from "next/navigation";
-// import { ManyContacts } from "@/app/components/server/contacts";
-// import {
-//   ManyRelComboFriendCriteria,
-//   ManyRelComboIrlCriteria,
-// } from "@/app/components/server/answers";
-// import { Suspense } from "react";
+import { fetchUserByUsername } from "@/app/lib/data/users";
+import { gatherContactByUserAndUsername } from "@/app/lib/data/contacts";
+import { notFound } from "next/navigation";
+import { ManyContacts } from "@/app/components/server/contacts";
+import {
+  ManyRelComboFriendCriteria,
+  ManyRelComboIrlCriteria,
+  ManyUserSharedToContactCustomAnswers,
+} from "@/app/components/server/answers";
+import { Suspense } from "react";
 import { ManyRelationCombinations } from "@/app/components/agnostic/lists";
 import { PageLink } from "@/app/components/agnostic/links";
 
@@ -25,42 +26,45 @@ export default async function NonePreviewPage({
   const username = params.username;
   const userLast = searchParams?.userlast || "";
   let relCombo = searchParams?.relcombo || "";
-  // const user = await fetchUserByUsername(username);
-  // const gatheredContact = await gatherContactByUserAndUsername(user, userLast);
+  const user = await fetchUserByUsername(username);
+  const gatheredContact = await gatherContactByUserAndUsername(user, userLast);
 
-  // if (gatheredContact &&
-  //   (gatheredContact.c1_kind === 'FRIEND' &&
-  //   gatheredContact.c2_kind === 'FRIEND' &&
-  //   gatheredContact.c1_blocking === false &&
-  //   gatheredContact.c2_blocking === false
-  //   )
-  //   ) {
-  //     relCombo = "friend"
-  // } if (gatheredContact &&
-  //   (gatheredContact.c1_kind === 'IRL' &&
-  //   gatheredContact.c2_kind === 'IRL' &&
-  //   gatheredContact.c1_blocking === false &&
-  //   gatheredContact.c2_blocking === false
-  //   )
-  //   ) {
-  //     relCombo = "irl"
-  // }
+  if (
+    relCombo === "" &&
+    gatheredContact &&
+    gatheredContact.c1_kind === "FRIEND" &&
+    gatheredContact.c2_kind === "FRIEND" &&
+    gatheredContact.c1_blocking === false &&
+    gatheredContact.c2_blocking === false
+  ) {
+    relCombo = "friend";
+  }
+  if (
+    relCombo === "" &&
+    gatheredContact &&
+    gatheredContact.c1_kind === "IRL" &&
+    gatheredContact.c2_kind === "IRL" &&
+    gatheredContact.c1_blocking === false &&
+    gatheredContact.c2_blocking === false
+  ) {
+    relCombo = "irl";
+  }
 
-  // if (!user) {
-  //   notFound();
-  // }
+  if (!user) {
+    notFound();
+  }
 
   return (
     <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
       <div className="max-w-prose text-center">
-        <h1>Welcome to {username}&apos;s Queried Previews.</h1>
+        {/* <h1>Welcome to {username}&apos;s Queried Previews.</h1>
         <p className="pt-2">
           Select a user you're acquainted with. (userlast in searchParams.)
         </p>
         {userLast !== "" && <p className="pt-2">userlast: {userLast}</p>}
         <ManyRelationCombinations />
-        {relCombo !== "" && <p className="pt-2">relcombo: {relCombo}</p>}
-        {/* <h1 className="font-semibold">
+        {relCombo !== "" && <p className="pt-2">relcombo: {relCombo}</p>} */}
+        <h1 className="font-semibold">
           Welcome to {user.user_app_wide_name}&apos;s Queried Previews.
         </h1>
         <ManyContacts user={user} />
@@ -76,8 +80,13 @@ export default async function NonePreviewPage({
         >
           {relCombo === "friend" && <ManyRelComboFriendCriteria user={user} />}
           {relCombo === "irl" && <ManyRelComboIrlCriteria user={user} />}
-          {(gatheredContact && (relCombo === "" || relCombo === "friend" || relCombo === "irl")) && <ManyUserSharedToContactCustomAnswers user={user} contact={gatheredContact} />}
-        </Suspense> */}
+          {gatheredContact && (relCombo === "friend" || relCombo === "irl") && (
+            <ManyUserSharedToContactCustomAnswers
+              user={user}
+              contact={gatheredContact}
+            />
+          )}
+        </Suspense>
         <PageLink href={`/users/${username}/previews`} name={"To Previews"} />
       </div>
     </main>
