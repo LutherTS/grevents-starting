4afd381ffa0eefb22677e7295bba279d7d3704a6
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-13 16:10:37 +0100

    Starting with the fake SELECT on FindFriendsPage.

---
+++ app/(pages)/users/[username]/friends/find/page.tsx
@@ -0,0 +1,31 @@
+// import { fetchUserByUsername } from "@/app/lib/data/users";
+// import { notFound } from "next/navigation";
+import { PageLink } from "@/app/components/agnostic/links";
+
+export default async function FindFriendsPage({
+  params,
+}: {
+  params: {
+    username: string;
+  };
+}) {
+  const username = params.username;
+  // const user = await fetchUserByUsername(username);
+
+  // if (!user) {
+  //   notFound();
+  // }
+
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
+      <div className="max-w-prose text-center">
+        <h1>Welcome to {username}&apos;s Find Friends.</h1>
+        {/* <h1 className="font-semibold">
+          Welcome to {user.user_app_wide_name}&apos;s Find Friends.
+        </h1> */}
+        <PageLink href={`/users/${username}/friends`} name={`See friends`} />
+        <PageLink href={`/users/${username}/previews`} name={`See previews`} />
+      </div>
+    </main>
+  );
+}
