ac9524a6e869f1eebebafe19b25eb1ebc6fdb5db
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-13 11:27:45 +0100

    It works, just the connection that messing up.

--- app/(pages)/users/[username]/personal-info/customized/page.tsx
+++ app/(pages)/users/[username]/personal-info/customized/page.tsx
@@ -1,11 +1,11 @@
-// import { fetchUserByUsername } from "@/app/lib/data/users";
-// import {
-//   ManyUserPseudonativeNotIrlCriteria,
-//   ManyUserPseudonativeIrlCriteria,
-//   ManyUserCustomCriteria,
-// } from "@/app/components/server/answers";
-// import { notFound } from "next/navigation";
-// import { Suspense } from "react";
+import { fetchUserByUsername } from "@/app/lib/data/users";
+import {
+  ManyUserPseudonativeNotIrlCriteria,
+  ManyUserPseudonativeIrlCriteria,
+  ManyUserCustomCriteria,
+} from "@/app/components/server/answers";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 import { PageLink } from "@/app/components/agnostic/links";
 
 export default async function CustomizedPage({
@@ -16,17 +16,17 @@ export default async function CustomizedPage({
   };
 }) {
   const username = params.username;
-  // const user = await fetchUserByUsername(username);
+  const user = await fetchUserByUsername(username);
 
-  // if (!user) {
-  //   notFound();
-  // }
+  if (!user) {
+    notFound();
+  }
 
   return (
     <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
       <div className="max-w-prose text-center">
-        <h1>Welcome to {username}&apos;s Customized Info.</h1>
-        {/* <h1 className="font-semibold">
+        {/* <h1>Welcome to {username}&apos;s Customized Info.</h1> */}
+        <h1 className="font-semibold">
           Welcome to {user.user_app_wide_name}&apos;s Customized Info.
         </h1>
         <Suspense
@@ -39,7 +39,7 @@ export default async function CustomizedPage({
           <ManyUserPseudonativeNotIrlCriteria user={user} />
           <ManyUserPseudonativeIrlCriteria user={user} />
           <ManyUserCustomCriteria user={user} />
-        </Suspense> */}
+        </Suspense>
         <PageLink
           href={`/users/${username}/personal-info/customized/modify-criteria`}
           name={"Modify"}
