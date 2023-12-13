cb7c852704849bbd785f7f0ed28bc1ff35424cb9
Luther Tchofo Safo <nintendobserver@gmail.com>
2023-12-13 08:34:07 +0100

    Nothing changed. :') But at least I've refactored and will continue.

--- app/(pages)/users/[username]/previews/queried/page.tsx
+++ app/(pages)/users/[username]/previews/queried/page.tsx
@@ -1,13 +1,15 @@
-// import { fetchUserByUsername } from "@/app/lib/data/users";
-// import { gatherContactByUserAndUsername } from "@/app/lib/data/contacts";
-// import { notFound } from "next/navigation";
-// // import { ManyContacts } from "@/app/components/server/contacts";
-// import {
-//   ManyRelComboFriendCriteria,
-//   ManyRelComboIrlCriteria,
-//   ManyUserSharedToContactCustomAnswers,
-// } from "@/app/components/server/answers";
-// import { Suspense } from "react";
+import { fetchUserByUsername } from "@/app/lib/data/users";
+import { gatherContactByUserAndUsername } from "@/app/lib/data/contacts";
+import { notFound } from "next/navigation";
+// import { ManyContacts } from "@/app/components/server/contacts"; // No longer concording with the expected user experience.
+import {
+  // ManyRelComboFriendCriteria,
+  // ManyRelComboIrlCriteria,
+  ManyRelComboFriendCriteriaTwo,
+  ManyRelComboIrlCriteriaTwo,
+  ManyUserSharedToContactCustomAnswers,
+} from "@/app/components/server/answers";
+import { Suspense } from "react";
 import { ManyRelationCombinations } from "@/app/components/agnostic/lists";
 import { relationCombinations } from "@/app/lib/utils/lists";
 import { PageLink } from "@/app/components/agnostic/links";
@@ -27,37 +29,37 @@ export default async function QueriedPreviewPage({
   const username = params.username;
   const userLast = searchParams?.userlast || "";
   let relCombo = searchParams?.relcombo || "";
-  // const user = await fetchUserByUsername(username);
-  // const gatheredContact = await gatherContactByUserAndUsername(user, userLast);
-  // if (
-  //   relCombo === "" &&
-  //   gatheredContact &&
-  //   gatheredContact.c1_kind === "FRIEND" &&
-  //   gatheredContact.c2_kind === "FRIEND" &&
-  //   gatheredContact.c1_blocking === false &&
-  //   gatheredContact.c2_blocking === false
-  // ) {
-  //   relCombo = "friend";
-  // }
-  // if (
-  //   relCombo === "" &&
-  //   gatheredContact &&
-  //   gatheredContact.c1_kind === "IRL" &&
-  //   gatheredContact.c2_kind === "IRL" &&
-  //   gatheredContact.c1_blocking === false &&
-  //   gatheredContact.c2_blocking === false
-  // ) {
-  //   relCombo = "irl";
-  // }
+  const user = await fetchUserByUsername(username);
+  const gatheredContact = await gatherContactByUserAndUsername(user, userLast);
+  if (
+    relCombo === "" &&
+    gatheredContact &&
+    gatheredContact.c1_kind === "FRIEND" &&
+    gatheredContact.c2_kind === "FRIEND" &&
+    gatheredContact.c1_blocking === false &&
+    gatheredContact.c2_blocking === false
+  ) {
+    relCombo = "friend";
+  }
+  if (
+    relCombo === "" &&
+    gatheredContact &&
+    gatheredContact.c1_kind === "IRL" &&
+    gatheredContact.c2_kind === "IRL" &&
+    gatheredContact.c1_blocking === false &&
+    gatheredContact.c2_blocking === false
+  ) {
+    relCombo = "irl";
+  }
 
-  // if (!user) {
-  //   notFound();
-  // }
+  if (!user) {
+    notFound();
+  }
 
   return (
     <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
       <div className="max-w-prose text-center">
-        <h1>Welcome to {username}&apos;s Queried Previews.</h1>
+        {/* <h1>Welcome to {username}&apos;s Queried Previews.</h1>
         <p className="pt-2">
           Select a user you're acquainted with. (userlast in searchParams.)
         </p>
@@ -73,8 +75,8 @@ export default async function QueriedPreviewPage({
               </p>
             )}
           </>
-        )}
-        {/* <h1 className="font-semibold">
+        )} */}
+        <h1 className="font-semibold">
           Welcome to {user.user_app_wide_name}&apos;s Queried Previews.
         </h1>
         <p className="pt-2">
@@ -108,15 +110,19 @@ export default async function QueriedPreviewPage({
             </>
           }
         >
-          {relCombo === "friend" && <ManyRelComboFriendCriteria user={user} />}
-          {relCombo === "irl" && <ManyRelComboIrlCriteria user={user} />}
+          {/* {relCombo === "friend" && <ManyRelComboFriendCriteria user={user} />}
+          {relCombo === "irl" && <ManyRelComboIrlCriteria user={user} />} */}
+          {relCombo === "friend" && (
+            <ManyRelComboFriendCriteriaTwo user={user} />
+          )}
+          {relCombo === "irl" && <ManyRelComboIrlCriteriaTwo user={user} />}
           {gatheredContact && (relCombo === "friend" || relCombo === "irl") && (
             <ManyUserSharedToContactCustomAnswers
               user={user}
               contact={gatheredContact}
             />
           )}
-        </Suspense> */}
+        </Suspense>
         <PageLink href={`/users/${username}/previews`} name={"To Previews"} />
       </div>
     </main>
