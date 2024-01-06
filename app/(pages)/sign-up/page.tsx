96904ce02913b0a8bc96729b15cec10c73cb8036
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-01-06 20:13:55 +0100

    Sign-In and Sign-Up pages created.

---
+++ app/(pages)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { H1 } from "@/app/components/agnostic/tags";
+
+export default async function SignUpPage() {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
+      <div className="max-w-prose text-center">
+        <H1>Welcome to the Sign-Up Page.</H1>
+      </div>
+    </main>
+  );
+}
