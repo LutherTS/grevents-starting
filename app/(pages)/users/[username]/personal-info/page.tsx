76f357ad1f51588494ca681aa0fab0c511af5015
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-01-08 11:03:26 +0100

    Thank you TypeScript.

--- app/(pages)/users/[username]/personal-info/page.tsx
+++ app/(pages)/users/[username]/personal-info/page.tsx
@@ -67,10 +67,12 @@ export default async function PersonalInfoPage({
   return (
     <main className="flex min-h-screen w-full items-center justify-center px-8 py-32">
       <div className="max-w-prose text-center">
-        {user.user_status_personal_info === "ANSWERUPDATED" && (
+        {(user.user_status_personal_info === "CUSTOMIZEDANSWERUPDATED" ||
+          user.user_status_personal_info === "STANDARDIZEDANSWERUPDATED") && (
           <AnswerValueUpdated user={user} />
         )}
-        {user.user_status_personal_info === "ANSWERDELETED" && (
+        {(user.user_status_personal_info === "CUSTOMIZEDANSWERUPDATED" ||
+          user.user_status_personal_info === "STANDARDIZEDANSWERUPDATED") && (
           <AnswerValueDeleted user={user} />
         )}
         {user.user_status_personal_info === "CRITERIAPINNED" && (
