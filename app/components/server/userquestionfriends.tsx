4508a47f31cdb2d0a9a30e4750623cdb7f0a5df6
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-01-14 04:48:27 +0100

    UserQuestionFriends agnostic.

--- app/components/server/userquestionfriends.tsx
+++ app/components/server/userquestionfriends.tsx
@@ -1,57 +1,11 @@
-// "use server";
+"use server";
 
-import {
-  // countUserQuestionFriends,
-  fetchAllUserQuestionFriends,
-} from "@/app/libraries/data/userquestionfriends";
+import { fetchAllUserQuestionFriends } from "@/app/libraries/data/userquestionfriends";
 import { UserQuestion } from "@/app/libraries/definitions/userquestions";
-import { UserQuestionFriend } from "@/app/libraries/definitions/userquestionfriends";
-import { ButtonCancelShareUserQuestionFriendForm } from "../client/forms";
-
-export function ManyUserQuestionFriendsLabel({
-  userQuestionFriends,
-}: {
-  userQuestionFriends: UserQuestionFriend[];
-}) {
-  return (
-    <>
-      {userQuestionFriends.length >= 2 ? (
-        <p className="mt-2 font-semibold text-zinc-500">
-          Shared with the following friends ({userQuestionFriends.length})
-        </p>
-      ) : (
-        <p className="mt-2 font-semibold text-zinc-500">
-          Shared with the following friend ({userQuestionFriends.length})
-        </p>
-      )}
-    </>
-  );
-}
-
-export function OneUserQuestionFriend({
-  userQuestion,
-  userQuestionFriend,
-}: {
-  userQuestion: UserQuestion;
-  userQuestionFriend: UserQuestionFriend;
-}) {
-  return (
-    <>
-      <div className="mt-2 flex justify-center">
-        <ButtonCancelShareUserQuestionFriendForm
-          userQuestion={userQuestion}
-          userQuestionFriend={userQuestionFriend}
-        />
-        <p>
-          <span className="font-semibold">
-            {userQuestionFriend.user_app_wide_name}
-          </span>{" "}
-          / {userQuestionFriend.user_username}
-        </p>
-      </div>
-    </>
-  );
-}
+import {
+  ManyUserQuestionFriendsLabel,
+  OneUserQuestionFriend,
+} from "../agnostic/userquestionfriends";
 
 export async function ManyUserQuestionFriends({
   userQuestion,
