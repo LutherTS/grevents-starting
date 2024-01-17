12b0441a8b1178a81fad34079dfd0ae0de85fa12
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-01-17 23:02:12 +0100

    ...It's going to be vanilla Tailwind.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+// import "@radix-ui/themes/styles.css";
+// import { Theme, ThemePanel } from "@radix-ui/themes";
+// import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +21,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      // suppressHydrationWarning
+    >
       <body className={inter.className}>
+        {/* <ThemeProvider attribute="class"> */}
         {/* <Theme> */}
         {children}
         {/* <ThemePanel /> */}
         {/* </Theme> */}
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
