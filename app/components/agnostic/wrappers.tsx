052e051a74d101fd208e88bcba56ea15fc973ecf
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-01-18 19:56:46 +0100

    Replaced gray with neutral.

--- app/components/agnostic/wrappers.tsx
+++ app/components/agnostic/wrappers.tsx
@@ -11,7 +11,7 @@ export function Main({ children }: { children: React.ReactNode }) {
 export function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="flex flex-col justify-center rounded-lg bg-transparent px-8 py-16 text-center sm:min-h-[70vh] sm:min-w-[55ch] sm:max-w-prose sm:bg-white sm:px-12 sm:dark:bg-gray-900">
+      <div className="flex flex-col justify-center rounded-lg bg-transparent px-8 py-16 text-center sm:min-h-[70vh] sm:min-w-[55ch] sm:max-w-prose sm:bg-white sm:px-12 sm:dark:bg-neutral-900">
         {children}
       </div>
     </>
